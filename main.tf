29d2ec8f633f4eb5464900dc9ffd382ae9cb374d
akihiro17 <7583548+akihiro17@users.noreply.github.com>
2025-12-15 02:12:57 +0900

    some changes

--- main.tf
+++ main.tf
@@ -85,7 +85,7 @@ resource "azurerm_function_app_flex_consumption" "example" {
   app_settings = {
     # workaround for the azure terraform provider issue
     # ref. https://github.com/hashicorp/terraform-provider-azurerm/issues/30732#issuecomment-3360715578
-    "AzureWebJobsStorage" = null
+    "AzureWebJobsStorage" = ""
 
     "AzureWebJobsStorage__accountName"    = azurerm_storage_account.log.name
     "AzureWebJobsStorage__blobServiceUri" = azurerm_storage_account.log.primary_blob_endpoint
@@ -115,6 +115,31 @@ resource "azurerm_role_assignment" "log_storage_for_function" {
   role_definition_name = "Storage Blob Data Owner"
   scope                = azurerm_storage_account.log.id
   principal_id         = azurerm_function_app_flex_consumption.example.identity[0].principal_id
+}
+
+resource "auth0_log_stream" "example" {
+  name   = "Azure logs"
+  type   = "eventgrid"
+  status = "active"
+
+  sink {
+    azure_region          = local.location
+    azure_resource_group  = azurerm_resource_group.example.name
+    azure_subscription_id = local.subscription_id
+  }
+
+  depends_on = [azurerm_eventgrid_partner_configuration.example]
+}
+
+resource "azurerm_eventgrid_partner_configuration" "example" {
+  resource_group_name                     = azurerm_resource_group.example.name
+  default_maximum_expiration_time_in_days = 7
+
+  partner_authorization {
+    # ref. https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/eventgrid_partner_configuration
+    partner_registration_id = "804a11ca-ce9b-4158-8e94-3c8dc7a072ec"
+    partner_name            = "Auth0"
+  }
 }
 
 data "azapi_resource" "partner_topic" {
@@ -167,6 +192,8 @@ data "archive_file" "function_zip" {
     ".vscode",
     "*.DS_Store",
     "local.settings.json",
+    "node_modules/**",
+    ".npm/**",
   ]
 }
 
